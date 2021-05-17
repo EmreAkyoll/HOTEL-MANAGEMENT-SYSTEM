ed350a545f6d69edb352376bd7f24946e4bce8ee
EmreAkyoll <78498301+EmreAkyoll@users.noreply.github.com>
2021-05-17 10:52:34 +0300

    Update HMSC.c

--- okul proje ödev/HMSC.c
+++ okul proje ödev/HMSC.c
@@ -5,6 +5,12 @@
 #include<windows.h>
 #include<stdlib.h>
 
+void add();  
+void list();
+void edit();  
+void delete1();
+void search();
+
 struct musteridetayi   //STRUCTURE DECLARATION
 {
 	char odanumarasi[10];
@@ -61,225 +67,6 @@ void giris()
 		getch();
 		
 	}
-}
-
-
-
-void ekle()
-{
-	FILE *f;
-	char test;
-	f=fopen("add.txt","a+");
-	if(f==0)
-	{   f=fopen("add.txt","w+");
-		system("cls");
-		printf("Lutfen biz bilgisayarinizda veritabanimizi kurarken bekleyiniz!!");
-		printf("\n Islem tamamlandi. Devam etmek icin herhangi bir tusa basiniz.!! ");
-		getch();
-	}
-	while(1)
-	{
-		system("cls");
-		printf("\n Musteri ayrintilarini giriniz :");
-		printf("\n**************************");
-		printf("\n Oda numarasini giriniz :\n");
-		scanf("\n%s",s.odanumarasi);
-		fflush(stdin);
-		printf("Isim giriniz :\n");
-		scanf("%s",s.isim);
-		printf("Adres giriniz :\n");
-		scanf("%s",s.adres);
-		printf("Telefon numarasini giriniz :\n");
-		scanf("%s",s.telefonnumarasi);
-		printf("Milliyetini giriniz :\n");
-		scanf("%s",s.milliyeti);
-		printf("E-postayi giriniz :\n");
-		scanf(" %s",s.e_posta);
-		printf("Sureyi giriniz : (\'x\'days):\n");
-		scanf("%s",&s.sure);
-		printf("Gelis tarihini giriniz : (dd\\mm\\yyyy):\n");
-		scanf("%s",&s.gelistarihi);
-		fwrite(&s,sizeof(s),1,f);
-		fflush(stdin);
-		printf("\n\nOda basariyla rezerve edildi.!!");
-		printf("\n Cikmak icin esc tusuna, baska bir musteri ayrintisi eklemek icin baska bir tusa basiniz.:");
-		test=getche();
-		if(test==27)
-			break;
-			
-	}
-	fclose(f);
-}
-
-
-
-void goster()
-{
-	FILE *f;
-	int i;
-	if((f=fopen("add.txt","r"))==NULL)
-		exit(0);
-	system("cls");
-	printf("ODA    ");
-	printf("ISIM\t ");
-	printf("\tADRES ");
-	printf("\tTELEFON NUMARASI ");
-	printf("\tMILLIYETI ");
-	printf("\tE_POSTA ");
-	printf("\t\t  SURE ");
-	printf("\t GELIS TARIHI \n");
-	
-	for(i=0;i<118;i++)
-		printf("-");
-	while(fread(&s,sizeof(s),1,f)==1)
-	{
-		/*printf("ODANUMARASİ :\t%s\n",s.odanumarasi);
-		printf("İSİM:\t%s\n",,s.isim);
-		printf("ADRES:\t%s\n",s.adres);
-		printf("TELEFONNUMARASİ:\t%s\n",s.telefonnumarasi);
-		printf("MİLLİYETİ \n");*/
-		printf("\n%s \t%s \t\t%s \t\t%s \t%s  \t%s  \t     %s  \t  %s",s.odanumarasi, s.isim , s.adres , s.telefonnumarasi ,s.milliyeti ,s.e_posta,s.sure,  s.gelistarihi);
-	}
-	printf("\n");
-	for(i=0;i<118;i++)
-		printf("-");
-
-	fclose(f);
-	getch();
-}
-
-
-
-void sil()
-{
-	FILE *f,*t;
-	int i=1;
-	char odanumarasi[20];
-	if((t=fopen("temp.txt","w"))==NULL)
-	exit(0);
-	if((f=fopen("add.txt","r"))==NULL)
-	exit(0);
-	system("cls");
-	printf("Veri tabanindan silinecek otelin Oda Numarasini giriniz: \n");
-	fflush(stdin);
-	scanf("%s",odanumarasi);
-	while(fread(&s,sizeof(s),1,f)==1)
-	{
-		if(strcmp(s.odanumarasi,odanumarasi)==0)
-		{       i=0;
-			continue;
-		}
-		else
-			fwrite(&s,sizeof(s),1,t);
-	}
-	if(i==1)
-	{       
-		printf("\n\n Bu Oda numarasindaki Musteri kayitlari bulunamadi!!");
-		//remove("E:/file.txt");
-	   //rename("E:/temp.txt","E:/file.txt");
-		getch();
-		fclose(f);
-		fclose(t);
-		main();
-	}
-	fclose(f);
-	fclose(t);
-	remove("add.txt");
-	rename("temp.txt","add.txt");
-	printf("\n\nMusteri basariyla kaldirildi....");
-	fclose(f);
-	fclose(t);
-	getch();
-}
-
-
-
-void ara()
-{
-system("cls");
-	FILE *f;
-	char odanumarasi[20];
-	int flag=1;
-	f=fopen("add.txt","r+");
-	if(f==0)
-		exit(0);
-	fflush(stdin);
-	printf("Ayrintilarini aramak icin musterinin oda numarasini girin: \n");
-	scanf("%s", odanumarasi);
-	while(fread(&s,sizeof(s),1,f)==1)
-	{
-		if(strcmp(s.odanumarasi,odanumarasi)==0){
-			flag=0;
-			printf("\n\t****Kayit Bulundu****\n ");
-			printf("\nOda numarasi :\t%s",s.odanumarasi);
-			printf("\nIsim:\t%s",s.isim);
-			printf("\nAdres:\t%s",s.adres);
-			printf("\nTelefon numarasi:\t%s",s.telefonnumarasi);
-			printf("\nMilliyeti:\t%s",s.milliyeti);
-			printf("\nE-Posta:\t%s",s.e_posta);
-			printf("\nSure:\t%s",s.sure);
-			printf("\nGelis Tarihi:\t%s",s.gelistarihi);
-			flag=0;
-			break;
-		}
-	}
-	if(flag==1){	
-		printf("\n\tIstenen Musteri bulunamadi!");
-	}
-	getch();
-	fclose(f);
-}
-
-
-
-void duzenle()
-{
-	FILE *f;
-	int k=1;
-	char odanumarasi[20];
-	long int size=sizeof(s);
-	if((f=fopen("add.txt","r+"))==NULL)
-		exit(0);
-	system("cls");
-	printf("Duzenlemek icin musterinin Oda numarasini girin:\n\n");
-	scanf("%[^\n]",odanumarasi);
-	fflush(stdin);
-	while(fread(&s,sizeof(s),1,f)==1)
-	{
-		if(strcmp(s.odanumarasi,odanumarasi)==0)
-		{
-			k=0;
-			printf("\nOda numarasini girin     :");
-			gets(s.odanumarasi);
-			printf("\nIsim giriniz.   :");
-			fflush(stdin);
-			scanf("%s",&s.isim);
-			printf("\nAdres giriniz.       :");
-			scanf("%s",&s.adres);
-			printf("\nTelefon numarasi giriniz. :");
-			scanf("%s",&s.telefonnumarasi);
-			printf("\nMilliyetinizi giriniz. :");
-			scanf("%s",&s.milliyeti);
-			printf("\nE-Postanizi giriniz :");
-			scanf("%s",&s.e_posta);
-			printf("\nSurenizi giriniz :");
-			scanf("%s",&s.sure);
-			printf("\nGelis tarihinizi giriniz :");
-			scanf("%s",&s.gelistarihi);
-			fseek(f,size,SEEK_CUR);  //istenen pozisyona gitmek icin
-			fwrite(&s,sizeof(s),1,f);
-			break;
-		}
-	}
-	if(k==1){
-		printf("\n\nKAYIT MEVCUT DEGIL!!!!");
-		fclose(f);
-		getch();
-	}else{
-	fclose(f);
-	printf("\n\n\t\tKAYDINIZ BASARIYLA DUZENLENDI!!!");
-	getch();
-}
 }
 
 
@@ -359,8 +146,218 @@ int main(){     // ANA FONKSİYON
 }
 
 
+void ekle()
+{
+	FILE *f;
+	char test;
+	f=fopen("add.txt","a+");
+	if(f==0)
+	{   f=fopen("add.txt","w+");
+		system("cls");
+		printf("Lutfen biz bilgisayarinizda veritabanimizi kurarken bekleyiniz!!");
+		printf("\n Islem tamamlandi. Devam etmek icin herhangi bir tusa basiniz.!! ");
+		getch();
+	}
+	while(1)
+	{
+		system("cls");
+		printf("\n Musteri ayrintilarini giriniz :");
+		printf("\n**************************");
+		printf("\n Oda numarasini giriniz :\n");
+		scanf("\n%s",s.odanumarasi);
+		fflush(stdin);
+		printf("Isim giriniz :\n");
+		scanf("%s",s.isim);
+		printf("Adres giriniz :\n");
+		scanf("%s",s.adres);
+		printf("Telefon numarasini giriniz :\n");
+		scanf("%s",s.telefonnumarasi);
+		printf("Milliyetini giriniz :\n");
+		scanf("%s",s.milliyeti);
+		printf("E-postayi giriniz :\n");
+		scanf(" %s",s.e_posta);
+		printf("Sureyi giriniz : (\'x\'days):\n");
+		scanf("%s",&s.sure);
+		printf("Gelis tarihini giriniz : (dd\\mm\\yyyy):\n");
+		scanf("%s",&s.gelistarihi);
+		fwrite(&s,sizeof(s),1,f);
+		fflush(stdin);
+		printf("\n\nOda basariyla rezerve edildi.!!");
+		printf("\n Cikmak icin esc tusuna, baska bir musteri ayrintisi eklemek icin baska bir tusa basiniz.:");
+		test=getche();
+		if(test==27)
+			break;
+			
+	}
+	fclose(f);
+}
 
 
+void goster()
+{
+	FILE *f;
+	int i;
+	if((f=fopen("add.txt","r"))==NULL)
+		exit(0);
+	system("cls");
+	printf("ODA    ");
+	printf("ISIM\t ");
+	printf("\tADRES ");
+	printf("\tTELEFON NUMARASI ");
+	printf("\tMILLIYETI ");
+	printf("\tE_POSTA ");
+	printf("\t\t  SURE ");
+	printf("\t GELIS TARIHI \n");
+	
+	for(i=0;i<118;i++)
+		printf("-");
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		/*printf("ODANUMARASİ :\t%s\n",s.odanumarasi);
+		printf("İSİM:\t%s\n",,s.isim);
+		printf("ADRES:\t%s\n",s.adres);
+		printf("TELEFONNUMARASİ:\t%s\n",s.telefonnumarasi);
+		printf("MİLLİYETİ \n");*/
+		printf("\n%s \t%s \t\t%s \t\t%s \t%s  \t%s  \t     %s  \t  %s",s.odanumarasi, s.isim , s.adres , s.telefonnumarasi ,s.milliyeti ,s.e_posta,s.sure,  s.gelistarihi);
+	}
+	printf("\n");
+	for(i=0;i<118;i++)
+		printf("-");
+
+	fclose(f);
+	getch();
+}
+
+
+void sil()
+{
+	FILE *f,*t;
+	int i=1;
+	char odanumarasi[20];
+	if((t=fopen("temp.txt","w"))==NULL)
+	exit(0);
+	if((f=fopen("add.txt","r"))==NULL)
+	exit(0);
+	system("cls");
+	printf("Veri tabanindan silinecek otelin Oda Numarasini giriniz: \n");
+	fflush(stdin);
+	scanf("%s",odanumarasi);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.odanumarasi,odanumarasi)==0)
+		{       i=0;
+			continue;
+		}
+		else
+			fwrite(&s,sizeof(s),1,t);
+	}
+	if(i==1)
+	{       
+		printf("\n\n Bu Oda numarasindaki Musteri kayitlari bulunamadi!!");
+		//remove("E:/file.txt");
+	   //rename("E:/temp.txt","E:/file.txt");
+		getch();
+		fclose(f);
+		fclose(t);
+		main();
+	}
+	fclose(f);
+	fclose(t);
+	remove("add.txt");
+	rename("temp.txt","add.txt");
+	printf("\n\nMusteri basariyla kaldirildi....");
+	fclose(f);
+	fclose(t);
+	getch();
+}
+
+
+void ara()
+{
+system("cls");
+	FILE *f;
+	char odanumarasi[20];
+	int flag=1;
+	f=fopen("add.txt","r+");
+	if(f==0)
+		exit(0);
+	fflush(stdin);
+	printf("Ayrintilarini aramak icin musterinin oda numarasini girin: \n");
+	scanf("%s", odanumarasi);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.odanumarasi,odanumarasi)==0){
+			flag=0;
+			printf("\n\t****Kayit Bulundu****\n ");
+			printf("\nOda numarasi :\t%s",s.odanumarasi);
+			printf("\nIsim:\t%s",s.isim);
+			printf("\nAdres:\t%s",s.adres);
+			printf("\nTelefon numarasi:\t%s",s.telefonnumarasi);
+			printf("\nMilliyeti:\t%s",s.milliyeti);
+			printf("\nE-Posta:\t%s",s.e_posta);
+			printf("\nSure:\t%s",s.sure);
+			printf("\nGelis Tarihi:\t%s",s.gelistarihi);
+			flag=0;
+			break;
+		}
+	}
+	if(flag==1){	
+		printf("\n\tIstenen Musteri bulunamadi!");
+	}
+	getch();
+	fclose(f);
+}
+
+
+void duzenle()
+{
+	FILE *f;
+	int k=1;
+	char odanumarasi[20];
+	long int size=sizeof(s);
+	if((f=fopen("add.txt","r+"))==NULL)
+		exit(0);
+	system("cls");
+	printf("Duzenlemek icin musterinin Oda numarasini girin:\n\n");
+	scanf("%[^\n]",odanumarasi);
+	fflush(stdin);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.odanumarasi,odanumarasi)==0)
+		{
+			k=0;
+			printf("\nOda numarasini girin     :");
+			gets(s.odanumarasi);
+			printf("\nIsim giriniz.   :");
+			fflush(stdin);
+			scanf("%s",&s.isim);
+			printf("\nAdres giriniz.       :");
+			scanf("%s",&s.adres);
+			printf("\nTelefon numarasi giriniz. :");
+			scanf("%s",&s.telefonnumarasi);
+			printf("\nMilliyetinizi giriniz. :");
+			scanf("%s",&s.milliyeti);
+			printf("\nE-Postanizi giriniz :");
+			scanf("%s",&s.e_posta);
+			printf("\nSurenizi giriniz :");
+			scanf("%s",&s.sure);
+			printf("\nGelis tarihinizi giriniz :");
+			scanf("%s",&s.gelistarihi);
+			fseek(f,size,SEEK_CUR);  //istenen pozisyona gitmek icin
+			fwrite(&s,sizeof(s),1,f);
+			break;
+		}
+	}
+	if(k==1){
+		printf("\n\nKAYIT MEVCUT DEGIL!!!!");
+		fclose(f);
+		getch();
+	}else{
+	fclose(f);
+	printf("\n\n\t\tKAYDINIZ BASARIYLA DUZENLENDI!!!");
+	getch();
+}
+}
 
 
 
